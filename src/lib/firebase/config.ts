515a8f5c6a584b0eab3bc21620e398ba82106e11
coder camp <itscodercamp@gmail.com>
2025-09-16 11:52:11 +0000

    is url ko add kr de https://9000-firebase-studio-1757767172056.cluster-

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -25,6 +25,11 @@ const firebaseConfig = {
 //
 // This code is set up for localhost development by default.
 // =================================================================
+
+// Dynamically set the authDomain for development environments
+if (typeof window !== 'undefined' && window.location.hostname !== 'localhost' && !window.location.hostname.endsWith('firebaseapp.com')) {
+  firebaseConfig.authDomain = window.location.hostname;
+}
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
