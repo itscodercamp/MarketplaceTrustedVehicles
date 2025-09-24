608f3dd06662ad1bb540e3e854fbd6bdf5dbd303
coder camp <itscodercamp@gmail.com>
2025-09-24 13:16:54 +0000

    Try fixing this error: `Runtime Error: ChunkLoadError: Loading chunk app

--- src/components/layout/pre-loader.tsx
+++ src/components/layout/pre-loader.tsx
@@ -1,15 +1,13 @@
 'use client';
-import Image from 'next/image';
 
 export default function PreLoader() {
   return (
     <div className="flex items-center justify-center h-screen bg-background">
-      <Image
+      <img
         src="https://media.giphy.com/media/WdzPgUxAazT2WNHA2r/giphy.gif"
         alt="Loading animation"
         width={200}
         height={200}
-        unoptimized
       />
     </div>
   );
